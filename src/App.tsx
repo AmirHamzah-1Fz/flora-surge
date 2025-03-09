a2379229a8a95ec200f52aa6eb6c4a4f0cc1014e
AmirHamzah133 <amirnation23@gmail.com>
2025-03-09 11:38:14 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <>
+      {/* HEADER */}
+
+      <main>
+        {/* MASUKKAN KOMPONEN LAINNYA DI DALAM MAIN KECUALI HEADER & FOOTER */}
+        <div className="text-7xl">Inisial Proyek</div>
+      </main>
+
+      {/* FOOTER */}
+    </>
+  );
+}
+
+export default App;
