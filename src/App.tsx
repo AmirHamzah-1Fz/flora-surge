d9dee68f233f495f54a100710eb9d676c7a506d5
AmirHamzah133 <amirnation23@gmail.com>
2025-03-12 13:34:01 +0700

    Initial Commit (REACT + TYPESCRIPT + VITE) Starter project.

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      {/* LETAKKAN FILE KOMPONEN DI DALAM MAIN (TIDAK TERMASUK HEADER & FOOTER) *DI BRANCH MAIN SAJA */}
+      <main className="">
+        <h1 className="text-5xl">Initial</h1>
+      </main>
+    </>
+  );
+}
+
+export default App;
