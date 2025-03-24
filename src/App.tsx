a3a11e87cf20306707f4944ff19853c9c266b7a3
AmirHamzah133 <amirnation23@gmail.com>
2025-03-24 11:16:14 +0700

    Update style Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import Footer from "./components/Footer";
+
 function App() {
   return (
     <>
       {/* LETAKKAN FILE KOMPONEN DI DALAM MAIN (TIDAK TERMASUK HEADER & FOOTER) *DI BRANCH MAIN SAJA */}
       <main className="">
-        <h1 className="text-5xl">Initial</h1>
       </main>
+
+      <Footer />
     </>
   );
 }
