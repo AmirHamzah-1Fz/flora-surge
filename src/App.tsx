667da0c16b7d1328c42e1a32062407697be891ba
Amir Hamzah <amirnation23@gmail.com>
2025-03-24 13:16:26 +0700

    Components/header (#35)
    * Branch untuk membuat branch baru (Fresh Branch)
    
    * UNIMPORTANT: files
    
    * Fixerror: config
    
    * Update fetch config
    
    * Stash header
    
    * Feature: Header, dropdown function, responsive
    
    * UNIMPORTANT: syntax

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import Header from "./components/Header";
 import Footer from "./components/Footer";
 import ChatBot from "./components/ChatBot";
+
 
 function App() {
   return (
     <>
       {/* LETAKKAN FILE KOMPONEN DI DALAM MAIN (TIDAK TERMASUK HEADER & FOOTER) *DI BRANCH MAIN SAJA */}
+
+      <Header />
       <main className="">
         <ChatBot />
       </main>
