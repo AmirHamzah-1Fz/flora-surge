4053509041f934d74c43e4ee78f6d819c8b597ec
AmirHamzah133 <amirnation23@gmail.com>
2025-03-24 08:33:54 +0700

    refactor: remove ChatMenu component from footer branch

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import ChatBot from "./components/ChatBot";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       {/* LETAKKAN FILE KOMPONEN DI DALAM MAIN (TIDAK TERMASUK HEADER & FOOTER) *DI BRANCH MAIN SAJA */}
       <main className="">
-        <ChatBot />
       </main>
+
+      <Footer />
     </>
   );
 }
