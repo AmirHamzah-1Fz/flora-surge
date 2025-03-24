5525a460354ab92a37972c5f168a1e20df99efba
Amir Hamzah <amirnation23@gmail.com>
2025-03-24 18:36:26 +0700

    Hero: Adjust mobile version
    * Update fetch config
    
    * Feature: Hero section initial commit
    
    * Adjust layout for mobile
    
    * Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
-import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Header from "./components/Header";
 import Footer from "./components/Footer";
 import ChatBot from "./components/ChatBot";
-
 
 function App() {
   return (
