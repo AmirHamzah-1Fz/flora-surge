c6cec74498a14ece0a578948cb1875ea88b8a148
AmirHamzah133 <amirnation23@gmail.com>
2025-03-24 14:16:00 +0700

    Feature: Hero section initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import Hero from "./components/Hero";
+
 function App() {
   return (
     <>
       {/* LETAKKAN FILE KOMPONEN DI DALAM MAIN (TIDAK TERMASUK HEADER & FOOTER) *DI BRANCH MAIN SAJA */}
       <main className="">
+        <Hero />
       </main>
     </>
   );
