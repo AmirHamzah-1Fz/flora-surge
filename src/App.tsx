7e636239ca89ab47f92b5a2416721a334f35f55c
AmirHamzah133 <amirnation23@gmail.com>
2025-03-24 11:38:29 +0700

    Stash header

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import Header from "./components/Header";
+
 function App() {
   return (
     <>
       {/* LETAKKAN FILE KOMPONEN DI DALAM MAIN (TIDAK TERMASUK HEADER & FOOTER) *DI BRANCH MAIN SAJA */}
+
+      <Header />
       <main className="">
       </main>
     </>
---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <header className="w-full fixed top-0 left-0 bg-white h-20">
+        <div className="flex justify-between items-center px-4 py-2">
+            
+        </div>
+    </header>
+  )
+}
+
+export default Header
