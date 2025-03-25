dab70b677f616a0324cb4154de8521ec00496e2f
Amir Hamzah <amirnation23@gmail.com>
2025-03-25 18:58:00 +0700

    Adjustment: native landing page colors and responsive
    * Update fetch config
    
    * Feature: Hero section initial commit
    
    * Adjust layout for mobile
    
    * Update App.tsx
    
    * Adjustment: native landing page colors

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -1,22 +1,26 @@
-import Bg from '../../assets/img/Background.png';
+import Bg from '../../assets/img/green-env.jpg';
 
 function Hero() {
   return (
     <section className="h-screen w-full">
       <div className="relative h-full w-full overflow-hidden">
+        <div className="absolute -z-[90] h-full w-full bg-gradient-to-br from-[var(--primary)]/80 from-60% to-[#000000]/60 max-md:to-[#000000]/20"></div>
         <img src={Bg} alt="Background" className="absolute -z-[100] h-full w-full object-cover brightness-[80%]" />
 
-        <div className="flex h-full w-full items-center justify-center mt-4 px-[4%] max-sm:px-[3%]">
+        <div className="mt-4 flex h-full w-full items-center justify-center px-[4%] max-sm:px-[3%]">
           <div className="flex h-auto w-full flex-col items-center justify-start text-center lg:max-w-4xl">
-            <h1 className="md:text-5xl text-4xl font-semibold text-[var(--green3)]">Karir Hijau untuk Masa Depan yang Sehat Berkelanjutan.</h1>
-            <p className="mt-4 max-w-[60ch] text-lg font-light max-sm:text-base text-[#ADBC9F]">Membangun karir dan ekonomi sehat yang berdampak positif bagi bumi dan generasi mendatang.</p>
+            <h1 className="text-4xl leading-14 font-semibold text-[var(--green3)] max-md:leading-normal max-sm:text-3xl md:text-5xl">Karir Hijau untuk Masa Depan yang Sehat Berkelanjutan.</h1>
+            <p className="mt-4 max-w-[60ch] text-lg font-light text-[#c0cdb5] max-sm:text-base">Membangun karir dan ekonomi sehat yang berdampak positif bagi bumi dan generasi mendatang.</p>
 
-            <div className="mt-10 flex max-[540px]:flex-col h-auto w-full items-start justify-center gap-4">
-              <button type="button" className="flex cursor-pointer max-[540px]:w-full items-center justify-center gap-3 rounded-full border border-[#FBFADA] bg-transparent px-6 py-4 font-semibold tracking-tight text-[#FBFADA] transition-colors duration-200">
-                Baca Selengkapnya
+            <div className="mt-10 flex h-auto w-full items-start justify-center gap-4 max-[540px]:flex-col">
+              <button
+                type="button"
+                className="flex cursor-pointer items-center justify-center gap-3 rounded-full border border-[#FBFADA] bg-transparent px-6 py-4 font-semibold tracking-tight text-[#FBFADA] transition-[color,scale] duration-300 ease-in-out hover:bg-[#FBFADA] hover:text-[var(--primary)] active:scale-95 max-[540px]:w-full max-sm:py-3 max-sm:text-sm"
+              >
+                <a href="/artikel-dan-berita">Baca Selengkapnya</a>
               </button>
-              <button type="button" className="flex cursor-pointer max-[540px]:w-full items-center justify-center gap-3 rounded-full border border-transparent bg-[#FBFADA] px-6 py-4 font-semibold tracking-tight text-[var(--primary)] transition-colors duration-200">
-                Mulai Pembelajaran
+              <button type="button" className="flex cursor-pointer items-center justify-center gap-3 rounded-full border border-transparent bg-[#FBFADA] px-6 py-4 font-semibold active:scale-95 tracking-tight text-[var(--primary)] transition-[colors,scale] duration-200 max-[540px]:w-full max-sm:py-3 max-sm:text-sm">
+                <a href="/pembelajaran">Mulai Pembelajaran</a>
                 <svg className="fill-[var(--primary)]" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor">
                   <path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h240q17 0 28.5 11.5T480-800q0 17-11.5 28.5T440-760H200v560h560v-240q0-17 11.5-28.5T800-480q17 0 28.5 11.5T840-440v240q0 33-23.5 56.5T760-120H200Zm560-584L416-360q-11 11-28 11t-28-11q-11-11-11-28t11-28l344-344H600q-17 0-28.5-11.5T560-800q0-17 11.5-28.5T600-840h200q17 0 28.5 11.5T840-800v200q0 17-11.5 28.5T800-560q-17 0-28.5-11.5T760-600v-104Z" />
                 </svg>
