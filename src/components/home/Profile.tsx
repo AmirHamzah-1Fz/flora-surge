28dd146072d90b27d9dbbcaf9506436dba80391d
AmirHamzah133 <amirnation23@gmail.com>
2025-04-10 11:38:45 +0700

    Feat: Add profile section

---
+++ src/components/home/Profile.tsx
@@ -0,0 +1,13 @@
+function Profile() {
+  return (
+    <section className="bg-light h-screen w-full">
+      <div className="container mx-auto h-auto w-full px-[4%] lg:max-w-5xl xl:max-w-6xl">
+        <div className="w-full pt-24">
+          <h1 className="text-primary mb-6 w-full text-center text-3xl font-bold">Mengapa Harus #FloraJobs?</h1>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Profile;
