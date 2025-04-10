591c391fb8924d23c430b950de39f84011ce68b4
Amir Hamzah <amirnation23@gmail.com>
2025-04-10 11:41:14 +0700

    Feat: Add initial of profile section (#71)
    * Feat: About section
    
    * Feat: Add profile section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,14 @@
 import ChatBot from "../components/ChatBot"
 import About from "../components/home/About"
 import Hero from "../components/home/Hero"
+import Profile from "../components/home/Profile"
 
 function Home() {
   return (
     <>
         <Hero />
         <About />
+        <Profile />
         <ChatBot />
     </>
   )
